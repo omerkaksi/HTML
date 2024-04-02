3240a3e5e313bfbcf1a695fd7272e9f8a9744f1a
omerkaksi <omerkaksi@yahoo.com>
2024-04-02 12:38:21 +0300

    js 10: User input

---
+++ js10/js/main.js
@@ -0,0 +1,16 @@
+//user input
+
+/* alert("Hello world!"); */
+/* let myBoolean=confirm("Ok===True\n Cancel=== False");
+console.log(myBoolean); */
+/* let name=prompt("enter your name");
+console.log(name ?? "You didn' enter your name"); */
+let userName=prompt("enter your name");
+if(userName){
+    console.log(userName.length);
+    console.log(userName.trim().length);
+    console.log(userName.trim());
+console.log(userName);
+}
+else {console.log("no name");
+}
