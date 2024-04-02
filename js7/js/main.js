b89c7e3b679d8f1547ba4dbf2b2cfd7919af40ed
omerkaksi <omerkaksi@yahoo.com>
2024-04-02 11:57:28 +0300

    javaScript: if else

---
+++ js7/js/main.js
@@ -0,0 +1,26 @@
+// If statements
+/* let customerIsBanned=false;
+let soup="chicken noodle soup";
+let reply;
+let crackers=true;
+if (customerIsBanned){
+    console.log("no soup");
+}
+else if (soup && crackers) {
+reply= `Here is your order of ${soup}`;
+}
+else{
+ reply=`Sorry no ${soup}`;
+}
+console.log(reply); */
+
+let testScore=81;
+let collegeStudent=true;
+let grade;
+if (testScore>=90){
+    grade="AA";
+}
+else if(testScore>=80){
+    grade="BB";
+}
+console.log(grade);
