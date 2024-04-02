29dd5091aa76e00079a97771dba664a99cda790c
omerkaksi <omerkaksi@yahoo.com>
2024-04-02 16:27:24 +0300

    javaScript 12: Loops

---
+++ js12/js/main.js
@@ -0,0 +1,24 @@
+//Loops
+let myNumber = 0;
+while(myNumber<=50){
+    console.log(myNumber);
+    myNumber++;
+} 
+let myName="cemile";
+let counter=3;
+let myLetter;
+myLetter=myName[counter];
+console.log(myLetter);
+//don't create an endless loop
+/* let myName="cemile"
+for(let i=0; i<=myName.length; i++){
+    console.log(myName.charAt(i));
+}
+/* let counter=0;
+let myLetter;
+while(true){
+myLetter=myName[counter];
+console.log(myLetter);
+if (myLetter=== "l") break;
+counter;
+} */
